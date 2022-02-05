647525d404b3d6ad03d326510f95f22adeb9df3c
Andrea Lin <andre93.lin@gmail.com>
2022-02-05 08:54:12 -0300

    login form added check to see not empty string

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
-import loginService from "./services/login";
-import Notification from "./components/Notification";
 import LoginForm from "./components/LoginForm";
 
 function App() {
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedUser");
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      setUser(user);
-    }
-  }, []);
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    console.log("login info", email, password);
-
-    try {
-      const user = await loginService.login({
-        email,
-        password,
-      });
-
-      window.localStorage.setItem("loggedUser", JSON.stringify(user));
-
-      setUser(user);
-      setEmail("");
-      setPassword("");
-    } catch (exception) {
-      setErrorMessage("");
-      setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
-    }
-  };
-
   return (
     <div>
       <h1>Restaurant Menu</h1>
-      <Notification message={errorMessage} />
 
       {/* LOGIN FORM */}
       {/* {user ? (
@@ -60,13 +20,7 @@ function App() {
         />
       )} */}
 
-      <LoginForm
-        handleLogin={handleLogin}
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setPassword={setPassword}
-      />
+      <LoginForm />
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,60 @@
+import { useState, useEffect } from "react";
+
+import loginService from "../services/login";
+
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
-const LoginForm = ({ handleLogin, email, setEmail, password, setPassword }) => {
+import Swal from "sweetalert2";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("login info", email, password);
+
+    if (email === "" || password === "") {
+      Swal.fire({
+        title: "Opps!",
+        text: "Please enter email and password",
+        icon: "error",
+        confirmButtonText: "Go back",
+      });
+    }
+
+    try {
+      const user = await loginService.login({
+        email,
+        password,
+      });
+
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
+
+      setUser(user);
+      setEmail("");
+      setPassword("");
+    } catch (exception) {
+      Swal.fire({
+        title: "Opps!",
+        text: "The email or password entered is incorrect",
+        icon: "error",
+        confirmButtonText: "Go back",
+      });
+    }
+  };
+
   return (
     <Container>
       <Form onSubmit={handleLogin}>
